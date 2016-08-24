9b0053c6b88a3ee60d99a41166f772b1a3cf8f15
Emma Milner <emma.milner@guardian.co.uk>
2016-08-24 11:47:36 +0100

    gets recipes from local db and dumps to ui (#13)

--- ui/app/com/gu/recipeasy/controllers/Application.scala
+++ ui/app/com/gu/recipeasy/controllers/Application.scala
@@ -5,9 +5,14 @@ import play.api.mvc._
 import com.gu.recipeasy.views
 import play.api.Configuration
 import play.api.libs.ws.WSClient
+import com.gu.recipeasy.db._
 
 class Application(override val wsClient: WSClient, override val conf: Configuration) extends Controller with AuthActions {
   def index = AuthAction {
-    Ok(views.html.app("Recipeasy"))
+    val newRecipe = DB.getNewRecipe
+    newRecipe match {
+      case Some(r) => Ok(views.html.app(r))
+      case None => NotFound
+    }
   }
 }
