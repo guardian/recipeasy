6d91f0a20136d073dded9dbb1b586b0bc184e0f1
Chris Birchall <chris.birchall@gmail.com>
2016-08-24 14:10:42 +0100

    Make DB a class and pass context to it (#14)
    This is so we can use the Play app's conf-magic-generated configuration
    to build the context.

--- ui/app/com/gu/recipeasy/controllers/Application.scala
+++ ui/app/com/gu/recipeasy/controllers/Application.scala
@@ -7,9 +7,9 @@ import play.api.Configuration
 import play.api.libs.ws.WSClient
 import com.gu.recipeasy.db._
 
-class Application(override val wsClient: WSClient, override val conf: Configuration) extends Controller with AuthActions {
+class Application(override val wsClient: WSClient, override val conf: Configuration, db: DB) extends Controller with AuthActions {
   def index = AuthAction {
-    val newRecipe = DB.getNewRecipe
+    val newRecipe = db.getNewRecipe
     newRecipe match {
       case Some(r) => Ok(views.html.app(r))
       case None => NotFound
