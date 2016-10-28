7d7a1a947212b69ff2f7e4a3279a61ccd46e5a24
Emma Milner <ekmilner@gmail.com>
2016-10-28 16:00:32 +0100

    removes extends anyval

--- common/src/main/scala/com/gu/recipeasy/db/ContextWrapper.scala
+++ common/src/main/scala/com/gu/recipeasy/db/ContextWrapper.scala
@@ -8,12 +8,11 @@ trait ContextWrapper {
   val config: TypesafeConfig
   lazy val dbContext = new JdbcContext[PostgresDialect, SnakeCase](config.getConfig("db.ctx")) with ImplicitQuery with Quotes
 
-  trait Quotes extends AnyVal {
+  trait Quotes {
     this: JdbcContext[_, _] =>
     implicit class DateTimeQuotes(left: OffsetDateTime) {
       def >(right: OffsetDateTime) = quote(infix"$left > $right".as[Boolean])
       def <(right: OffsetDateTime) = quote(infix"$left < $right".as[Boolean])
     }
   }
-
 }
