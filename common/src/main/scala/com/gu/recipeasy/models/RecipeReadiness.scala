666e9f7bc89a6d585ed857fe3794a65f1a1c50de
Pascal Honoré <pascal@alseyn.net>
2016-11-29 18:36:28 +0000

    Refactoring

--- common/src/main/scala/com/gu/recipeasy/models/RecipeReadiness.scala
+++ common/src/main/scala/com/gu/recipeasy/models/RecipeReadiness.scala
@@ -12,13 +12,7 @@ object RecipeReadiness {
     recipes.size
   }
   def selectNigelSlaterRecipes(db: DB): List[Recipe] = {
-    def recipeIsNigelSlaterRecipe(recipe: Recipe): Boolean = {
-      recipe.credit match {
-        case None => false
-        case Some(credit) => (credit.contains("Nigel") && credit.contains("Slater"))
-      }
-    }
-    db.getRecipes().filter(recipe => recipeIsNigelSlaterRecipe(recipe))
+    db.getRecipes().filter(_.credit.exists(_.contains("Nigel Slater")))
   }
   def migrateNigelSlaterRecipes(db: DB): Int = {
     val recipes: List[Recipe] = selectNigelSlaterRecipes(db)
