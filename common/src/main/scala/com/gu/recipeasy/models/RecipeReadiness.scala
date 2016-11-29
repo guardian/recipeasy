ad4c5e86dc9bc4fe015326a83d4832959491262f
Pascal Honoré <pascal@alseyn.net>
2016-11-29 15:14:33 +0000

    Last trivial migration

--- common/src/main/scala/com/gu/recipeasy/models/RecipeReadiness.scala
+++ common/src/main/scala/com/gu/recipeasy/models/RecipeReadiness.scala
@@ -3,11 +3,11 @@ package com.gu.recipeasy.models
 import com.gu.recipeasy.db.DB
 
 object RecipeReadiness {
-  def selectRecipes(db: DB): List[Recipe] = {
-    db.getRecipes().filter(recipe => ((recipe.ingredientsLists.lists.size >= 2) && (recipe.status == New)))
+  def selectNewRecipesWithNonEmptyIngredientLists(db: DB): List[Recipe] = {
+    db.getRecipes().filter(recipe => ((recipe.ingredientsLists.lists.size > 0) && (recipe.status == New)))
   }
   def updateRecipesReadiness(db: DB): Int = {
-    val recipes: List[Recipe] = selectRecipes(db)
+    val recipes: List[Recipe] = selectNewRecipesWithNonEmptyIngredientLists(db)
     recipes.foreach(recipe => db.setOriginalRecipeStatus(recipe.id, Ready))
     recipes.size
   }
