f8fd7e1ee0281e24ccb13e1596d2286b0f15bdfc
Pascal Honoré <pascal@alseyn.net>
2017-01-03 16:50:35 +0000

    Update tests

--- ui/test/com/gu/recipeasy/ApplicationSpec.scala
+++ ui/test/com/gu/recipeasy/ApplicationSpec.scala
@@ -9,11 +9,11 @@ class recipeConversion extends FlatSpec with Matchers {
 
     val toast = new IngredientsList(Some("toast"), Seq("bread", "toaster"))
     val porridge = new IngredientsList(Some("porridge"), Seq("cup of oats", "glug of milk", "banana"))
-    val bread = new DetailedIngredient(None, None, "", None, "bread")
-    val toaster = new DetailedIngredient(None, None, "", None, "toaster")
-    val oats = new DetailedIngredient(None, None, "", None, "cup of oats")
-    val milk = new DetailedIngredient(None, None, "", None, "glug of milk")
-    val banana = new DetailedIngredient(None, None, "", None, "banana")
+    val bread = new DetailedIngredient(None, None, None, None, "", None, "bread")
+    val toaster = new DetailedIngredient(None, None, None, None, "", None, "toaster")
+    val oats = new DetailedIngredient(None, None, None, None, "", None, "cup of oats")
+    val milk = new DetailedIngredient(None, None, None, None, "", None, "glug of milk")
+    val banana = new DetailedIngredient(None, None, None, None, "", None, "banana")
 
     val detailedToast = new DetailedIngredientsList(Some("toast"), Seq(bread, toaster))
     val detailedPorridge = new DetailedIngredientsList(Some("porridge"), Seq(oats, milk, banana))
