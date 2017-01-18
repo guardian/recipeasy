bae6f175eb451516ced3204fec5a483122553968
Anne Byrne <anne.byrne@guardian.co.uk>
2017-01-18 15:55:23 +0000

    Remove unnecessary inherited of from ingredient item

--- ui/public/javascript/editForm.js
+++ ui/public/javascript/editForm.js
@@ -58,8 +58,8 @@ function guessItem(){
                     $(this).siblings(".ingredient__detail__quantity").val(1)
                }
               //remove known units
-              var cleanGuess = itemGuess[0].replace(/\b(cup|g|kg|oz|lb|bottle|floz|l|litre|ml|tsp|tbsp|dsp|bunch|cm|can|clove|dash|grating|handful|packet|piece|pinch|sheet|sprig|stick)e?s?\b\s?/g, "").replace(/\ba\b/i, "")
-              $(this).val(cleanGuess)
+              var cleanGuess = itemGuess[0].replace(/\b(cup|g|kg|oz|lb|bottle|floz|l|litre|ml|tsp|tbsp|dsp|bunch|cm|can|clove|dash|grating|handful|packet|piece|pinch|sheet|sprig|stick)e?s?\b\s?/g, "").replace(/\ba\b/i, "").replace(/^(\s)?\bof\b/i, "")
+                $(this).val(cleanGuess)
             }
         }
     })
